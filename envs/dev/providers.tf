bf92c8c5d08c5d024d8461a49d34aac371ada64d
ssoft.tou <kk.tou@startiasoft.co.jp>
2024-10-30 15:27:52 +0900

    update terraform-docs markdown

--- envs/dev/providers.tf
+++ envs/dev/providers.tf
@@ -1,4 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"  # Allow versions 5.x
+    }
+  }
+}
+
 provider "aws" {
   region = var.aws_region
 }
-
